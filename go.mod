6442fdbb359c51f229d52fe31074b004c1ee058b
Jose Vazquez <josvaz@protonmail.com>
2026-03-10 09:35:49 +0100

    Use mage

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.7
 
 require (
 	github.com/fsnotify/fsnotify v1.9.0
+	github.com/magefile/mage v1.16.0
 	github.com/miekg/dns v1.1.72
 	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.11.1
---
+++ magefiles/mage.go
@@ -0,0 +1,28 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/magefile/mage/sh"
+)
+
+// Build compiles the project binary into the bin folder.
+func Build() error {
+	fmt.Println("Building binary...")
+	return sh.RunV("go", "build", "-o", "bin/locals", "./main.go")
+}
+
+// Test runs all unit tests in the project.
+func Test() error {
+	fmt.Println("Running tests...")
+	return sh.RunV("go", "test", "-v", "./...")
+}
+
+// Clean removes the build artifacts.
+func Clean() error {
+	fmt.Println("Cleaning up...")
+	return sh.Rm("bin")
+}
