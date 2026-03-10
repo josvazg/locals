bded6f8cd23f3c8a2464f1003a1c8b938f6367a7
Jose Vazquez <josvaz@protonmail.com>
2026-03-10 09:36:12 +0100

    Replace shell.nix by flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Multi-arch dev environment for locals";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        unstable = import nixpkgs-unstable { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "locals";
+
+          buildInputs = with pkgs; [
+            curl
+            git
+            mage
+            shellcheck
+            neovim
+            mkcert
+            unstable.go
+          ];
+
+          shellHook = ''
+            export PATH=$PATH:$(go env GOPATH)/bin
+            export EDITOR=nvim
+            export CGO_CFLAGS="-O2"
+          '';
+        };
+      }
+    );
+}
+
